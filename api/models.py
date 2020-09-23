db59ca778b0b1cb0f6b3a8de42e00944d999ebdb
kazukichi <zexyl_x@icloud.com>
2020-09-23 23:16:03 +0900

    Profileモデル、Postモデル、Commentモデル定義

--- api/models.py
+++ api/models.py
@@ -7,6 +7,25 @@ from django.contrib.auth.models import (
 from django.conf import settings
 
 
+# アバター画像アップロード時拡張子取得及びファイル名作成
+def upload_avatar_path(instance, filename):
+    ext = filename.split(".")[-1]
+    return "/".join(
+        [
+            "avatars",
+            str(instance.userProfile.id) + str(instance.nickName) + str(".") + str(ext),
+        ]
+    )
+
+
+# 投稿画像アップロード時拡張子取得及びファイル名作成
+def upload_post_path(instance, filename):
+    ext = filename.split(".")[-1]
+    return "/".join(
+        ["posts", str(instance.userPost.id) + str(instance.title) + str(".") + str(ext)]
+    )
+
+
 # username認証からemail認証に変更するため、BaseUserManagerをオーバーライド
 class UserManager(BaseUserManager):
 
@@ -53,3 +72,51 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+# Profileモデルを定義
+class Profile(models.Model):
+    nickName = models.CharField(max_length=20)
+    # 一対一及び対象のユーザーが削除された場合userProfileも連動して削除
+    userProfile = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name="userProfile", on_delete=models.CASCADE
+    )
+    # 初回登録時日時は自動で登録
+    created_on = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(blank=True, null=True, upload_to=upload_avatar_path)
+
+    def __str__(self):
+        return self.nickName
+
+
+# Postモデル定義
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    # 投稿者(一対多及び対象のユーザーが削除された場合userPostも連動して削除)
+    userPost = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="userPost", on_delete=models.CASCADE
+    )
+    # 初回登録時日時は自動で登録
+    created_on = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(blank=True, null=True, upload_to=upload_post_path)
+    # 多対多
+    liked = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="liked", blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+# コメントモデル定義
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    # どのユーザーに対してのコメントなのか(一対多及び対象のユーザーが削除された場合userCommentも連動して削除)
+    userComment = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="userComment", on_delete=models.CASCADE
+    )
+    # 一対多及び対象のユーザーが削除された場合userCommentも連動して削除
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
